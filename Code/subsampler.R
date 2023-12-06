088c1b1603eac3ccd0772c23edfd41edb9f2d85e
Rhys Inward <67955642+rhysinward@users.noreply.github.com>
2023-12-06 10:18:37 +0000

    Add files via upload

--- Code/subsampler.R
+++ Code/subsampler.R
@@ -1,6 +1,7 @@
 ## load packages
 required_packages <- c("optparse", "dplyr","lubridate","tidyr",
-                       "readr","ape","seqinr","countrycode")
+                       "readr","ape","seqinr","countrycode","ggplot2",
+                       "ggplot2","purrr")
 suppressMessages(
   for (package in required_packages) {
     if (!require(package, character.only = TRUE)) {
@@ -16,7 +17,9 @@ opt_parser <- OptionParser(
     make_option(c("-m", "--metadata"), type="character", help="Input tsv file containing metadata from GenBank, with sequence identifiers matching those in the input fasta file."),
     make_option(c("-f", "--fasta"), type="character", help="Input fasta file, with sequence identifiers matching those in the metadata file."),
     make_option(c("-c", "--country"), type="character", help="Country(ies) of Interest, the first country specified will determine local region"),
-    make_option(c("-n", "--number_sequences"), type="character", help="Number of desired sequences (defult is 1:1 ratio with number of sequences from desired country(ies))")
+    make_option(c("-n", "--number_sequences"), type="numeric", help="Number of desired sequences (defult is 1:1 ratio with number of sequences from desired country(ies))"),
+    make_option(c("-e", "--prop_rd"), type="numeric",default = 0.8, help="Set proportion desired for region of interest (defult is 0.8)"),
+    make_option(c("-w", "--prop_or"), type="numeric",default = 0.2, help="Set proprotion desired for outside region of interest (defult is 0.2)")
   )
   )
 
@@ -31,7 +34,7 @@ opt = parse_args(opt_parser)
 ##########################################################
 ## read in input metadata file
 if (!is.null(opt$metadata)) {
-  metadata.df <- read.csv("results/Dengue_1_infoTbl.csv")
+  metadata.df <- read.csv(opt$metadata)
 } else {
   cat("Input metadata file. Exiting now...")
   quit()
@@ -62,10 +65,6 @@ metadata.df <- metadata.df %>%
 metadata.df <- metadata.df %>%
   mutate(Year = year(ymd(Date)))
 
-metadata.df <- metadata.df %>%
-  dplyr :: mutate(RowID = row_number())
-
-
 # Check if any rows were removed
 if (nrow(metadata.df) < length) {
   cat("Some identifiers in the metadata did not match the FASTA file. They have been removed.\n")
@@ -85,7 +84,10 @@ metadata.df <- metadata.df %>%
 metadata.df$Continent <- countrycode(metadata.df$Country, origin = "country.name", destination = "continent")
 
 #select all metadata from country of interest is ok if there are no metadata but number of required sequences needs to be specified
-countries_of_interest <-   opt$country
+
+countries_of_interest <- opt$country
+
+#opt$country
 
 countries_of_interest_first <- strsplit(countries_of_interest, ",")[[1]]
 
@@ -93,22 +95,23 @@ metadata_countries_of_interest <- metadata.df %>%
   filter(Country %in% countries_of_interest)
 
 ##############################################################################
-# Step 1: Identify the region of the first country
+# Step 3: Identify the region of the first country
 ##############################################################################
 
 first_country <- countries_of_interest_first[1]
 first_country_region <- countrycode(first_country, origin = "country.name", destination = "continent")
 
 ##############################################################################
-# Step 2: Input required number of sequences and calculate Sequences per Year
+# Step 4: Input required number of sequences and calculate Sequences per Year
 ##############################################################################
 
-#if opt$number_sequences is null (I.e. not inputed then automatically select 1:1 ratio) if not subtract 
+
+#if opt$number_sequences is null (I.e. not imputed then automatically select 1:1 ratio) if not subtract 
 #required number of sequences from number of sequences from countries of interest
-if (is.null(3000)) {
-  n_remainder <- as.numeric(nrow(metadata_countries_of_interest) * 2)
+if (is.null(opt$number_sequences)) {
+  n_remainder <- as.numeric(nrow(metadata_countries_of_interest))
 } else {
-  n_remainder <- as.numeric(3000) - nrow(metadata_countries_of_interest) # Total number of sequences desired
+  n_remainder <- as.numeric(opt$number_sequences) - nrow(metadata_countries_of_interest) # Total number of sequences desired
 }
 
 if (n_remainder < 0) {
@@ -117,163 +120,148 @@ if (n_remainder < 0) {
        nrow(metadata_countries_of_interest), ").")
 }
 
-num_years <- length(unique(metadata.df$Year)) # Number of years
+total_proportion <- opt[["prop_rd"]] + opt[["prop_or"]]
+
+# Check if the total proportion is not equal to 1
+if (total_proportion != 1) {
+  error_message <- sprintf("Error: The total proportion of 'prop_RD' and 'prop_OR' should be 1, but it is %.2f", total_proportion)
+  stop(error_message)
+}
 
 # In-region sequences per year
-in_region_sequences <- round(0.8 * n_remainder)
+in_region_sequences <- round(opt[["prop_rd"]] * n_remainder)
 # Out-of-region sequences per year
-out_region_sequences <- round(0.2 * n_remainder)
+out_region_sequences <- round(opt[["prop_or"]] * n_remainder)
 
 ##############################################################################
 # Step 3: Sampling Sequences per Country within a Region
 ##############################################################################
-
-non_selected_country_metadata <- metadata.df %>%
-  filter(!Country %in% countries_of_interest)
   
-in_region_countries <- unique(non_selected_country_metadata$Country[non_selected_country_metadata$Continent == first_country_region])
+in_region_countries <- unique(metadata.df$Country[metadata.df$Continent == first_country_region])
 
-# Initialize variables
-finished_sampling <- FALSE
+same_region_metadata <- metadata.df %>%
+  filter(!Country %in% countries_of_interest & Country %in% in_region_countries)
 
-# Create a function to sample one sequence for each entry in countries_vector
-library(dplyr)
+#sample sequences from countries of interest
 
-sample_sequences_in <- function(metadata, countries_vector,end_year, start_year, total_required) {
-  # Initialize an empty dataframe to store the sampled sequences
-  sampled_sequences <- data.frame()
+# Function to perform subsampler
+
+subsampler <- function(df, max_per_year) {
+  if (max_per_year < 1) {
+    sampled_data <- data.frame()
+} else {
+  unique_years <- unique(df$Year)
+  unique_countries <- unique(df$Country)
+  sampled_data <- data.frame()
   
-  num_sequences_per_year <- in_region_sequences / (end_year - start_year + 1)  
-  
-  countries_with_data <- unique(metadata$Country[metadata$Year == year])
-  countries_with_data <- intersect(countries_with_data, shuffled_countries)  
-  countries_vector <- rep(countries_with_data, length.out = 500)
-  
-  for (year in start_year:end_year) {
-    # Initialize a count for the number of sequences sampled for the current year
-    sequences_sampled_this_year <- 0
+  for (year in unique_years) {
+    year_data <- filter(df, Year == year)
+    count <- 0
     
-    # Iterate over the countries vector
-    for (country in countries_vector) {
-      # Stop the inner loop if the required number of sequences for the year is reached
-      if (sequences_sampled_this_year >= num_sequences_per_year) {
-        break
-      }
-  
-    # Check if the country has some data for the specified year
-    if (country %in% countries_vector) {
-      # Filter the metadata for the current country and year
-      sequences <- metadata %>%
-        filter(Country == country, Year == year) 
-      
-      # Sample 1 sequence if available
-      if (nrow(sequences) >= 1) {
-        sampled_sequence <- sequences %>% 
-          slice_sample(n = 1)
-        
-        # Bind the sampled sequence to the sampled_sequences dataframe
-        sampled_sequences <- rbind(sampled_sequences, sampled_sequence)
-        metadata <- metadata[setdiff(1:nrow(metadata), which(metadata$Sequence_name == sampled_sequence$Sequence_name)),]
-        sequences_sampled_this_year <- sequences_sampled_this_year + 1
-        
+    while(count < max_per_year && nrow(year_data) > 0) {
+      unique_countries <- sample(unique_countries)  # Randomize the order of countries so same countries aren't always sampled first
+      for (country in unique_countries) {
+        country_data <- filter(year_data, Country == country)
+        if (nrow(country_data) > 0) {
+          sampled_data <- rbind(sampled_data, slice_sample(country_data, n = 1))
+          count <- count + 1
+          if (count == max_per_year) break
         }
-      # If no sequence is available, do nothing and move to the next country
-    }
+      }
+      suppressMessages({
+        year_data <- anti_join(year_data, sampled_data)
+      })
+          }
   }
-  
-#  if (nrow(sampled_sequences) < round(0.8 * total_required)) {
-#    additional_needed <- round(0.8 * total_required) - nrow(sampled_sequences)
-#    additional_sequences <- metadata %>%
-#      filter(Country %in% countries_with_data) %>%
-#      slice_sample(n = additional_needed, replace = FALSE)
-#    sampled_sequences <- rbind(sampled_sequences, additional_sequences)
-#  }
-    
-    if (nrow(sampled_sequences) >= total_required) {
-      break
-    }
+  if (nrow(sampled_data) < max_per_year * length(unique_years)) {
+    remaining <- max_per_year * length(unique_years) - nrow(sampled_data)
+    additional_data <- df %>%
+      filter(!Sequence_name %in% sampled_data$Sequence_name) %>%
+      slice_sample(n = remaining)
+    sampled_data <- rbind(sampled_data, additional_data)
   }
-  
-  return(sampled_sequences)
+  sampled_data
+}
 }
 
-
-sampled_sequences_list <- list()
-
-for (year in sort(unique(non_selected_country_metadata$Year))) {
-  # Shuffle countries to give each one an equal chance in the sampling process
-  shuffled_countries <- sample(in_region_countries)
-  sampled_sequences <- sample_sequences(non_selected_country_metadata, shuffled_countries, max(non_selected_country_metadata$Year), min(non_selected_country_metadata$Year),in_region_sequences)
-  sampled_sequences_list[[length(sampled_sequences_list) + 1]] <- sampled_sequences
-}
-
-sampled_sequences_within_region <- bind_rows(sampled_sequences_list)
+# Apply advanced sampling to the data
+sampling_region_of_interest <- subsampler(same_region_metadata, max_per_year = round(in_region_sequences/length(unique(same_region_metadata$Year))))
 
 ##############################################################################
 # Step 4: Sampling Sequences per Country within a Region
 ##############################################################################
+in_region_countries <- unique(metadata.df$Country[metadata.df$Continent == first_country_region])
 
-out_of_region_countries <- unique(non_selected_country_metadata$Country[non_selected_country_metadata$Continent != first_country_region])
+out_of_region_countries <- unique(metadata.df$Country[metadata.df$Continent != first_country_region])
 
-# Initialize variables
-finished_sampling <- FALSE
+out_of_region_metadata <- metadata.df %>%
+  filter(!Country %in% countries_of_interest & Country %in% out_of_region_countries)
 
-# Initialize a list to store the sampled sequences
-sampled_sequences_list <- list()
-
-for (year in sort(unique(non_selected_country_metadata$Year))) {
-  # Shuffle countries to give each one an equal chance in the sampling process
-  shuffled_countries <- sample(out_of_region_countries)
-  sampled_sequences <- sample_sequences(non_selected_country_metadata, shuffled_countries, max(non_selected_country_metadata$Year), min(non_selected_country_metadata$Year),out_region_sequences)
-  sampled_sequences_list[[length(sampled_sequences_list) + 1]] <- sampled_sequences
-}
-
-sampled_sequences_out_of_region <- bind_rows(sampled_sequences_list)
+# Apply advanced sampling to the data
+sampling_out_of_region <- subsampler(out_of_region_metadata, max_per_year = round(out_region_sequences/length(unique(out_of_region_metadata$Year))))
 
 ##############################################################################
 # Step 5: Combine all sampled sequences into a single data frame
 ##############################################################################
-final_sample <- rbind(metadata_countries_of_interest,sampled_sequences_within_region,sampled_sequences_out_of_region)
+# Assuming metadata_countries_of_interest, sampling_out_of_region, and sampling_region_of_interest are already defined
+
+final_sample <- metadata_countries_of_interest
+
+#if metadata is empty do not join
+
+if (nrow(sampling_out_of_region) == 0 && nrow(sampling_region_of_interest) == 0) {
+} else {
+  if (nrow(sampling_out_of_region) > 0) {
+    final_sample <- rbind(final_sample, sampling_out_of_region)
+  }
+  if (nrow(sampling_region_of_interest) > 0) {
+    final_sample <- rbind(final_sample, sampling_region_of_interest)
+  }
+}
+
+##############################################################################
+# Step 6: Combine all sampled sequences into a single data frame
+##############################################################################
+
+#get count of data 
+
+final_sample_plot <- final_sample %>%
+  mutate(Group = if_else(Country == first_country, first_country, as.character(Continent))) %>%
+  group_by(Group, Year) %>%
+  summarise(count = n()) %>%
+  ungroup()
+
+#plot data with good color scheme
+
+ggplot(final_sample_plot, aes(x = Year, y = count, fill = Group)) +
+  geom_bar(stat = "identity", position = "stack") +
+  labs(x = "Year", y = "Number of Sequences", title = "Number of Sequences per Year") +
+  theme_bw() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  scale_fill_brewer(palette = "Set1")
+
+#export to pdf
+
+serotype <- unique(final_sample$Serotype)
+
+ggsave(paste0('results/subsampled_', serotype, '_summary_plot.pdf'), width = 10, height = 10, units = "in")
+
+
 ##############################################################################
 # Step 6: Match sample to sequences and produce sampled metadata + sequences
 ##############################################################################
 
-row_ID <- as.numeric(final_sample$RowID)
-
-keep <- metadata.df %>%
-  filter(RowID %in% row_ID)
-
-
-
-serotype <- unique(metadata.df$Serotype)
-
 colnames(final_sample) <- c('GenBank_ID', "Country", "State",
-                          "City", "Serotype", "Sequence_Type", "date","Decimal_Date","name","Year","Continent")
+                            "City", "Serotype", "date","Decimal_Date","name","Year","Continent")
 
-final_sample <- select(final_sample,c(9,7,1,2,3,4,5,6,8,10,11))
-
-write.table(final_sample,
-            file = paste0("results/subsampled_",serotype,'_infoTbl.txt'),
-            sep = "\t", 
-            col.names = TRUE, 
-            row.names = FALSE, 
-            quote = FALSE)
-
-write.csv(final_sample,
-          file = paste0("results/subsampled_",serotype,'_infoTbl.csv'),
-          row.names = FALSE)
-
-check <- read.csv(paste0("results/subsampled_",serotype,'_infoTbl.txt'),sep ="")
 seq_name <- as.data.frame(as.matrix(attributes(seqs)$names))
 
-not_in_seq_name <- anti_join(metadata.df, final_sample, by = c("GenBank_ID" = "GenBank_ID"))
-
-keep <- metadata.df %>%
-  filter(GenBank_ID %in% final_sample$GenBank_ID)
+keep <- seq_name %>%
+  filter(V1 %in% final_sample$name)
 
 taxa_split <- data.frame(do.call('rbind',strsplit(as.character(keep$V1),'|',fixed = TRUE)))
-
-species.to.keep <- not_in_seq_name$name
+taxa_split$name <- keep$V1
+species.to.keep <- taxa_split$name
 
 vec.names<-unlist(lapply(strsplit(names(seqs), ";"), function(x)x[length(x)]))
 
@@ -281,25 +269,20 @@ vec.tokeep <-which(vec.names %in%  species.to.keep)
 
 # Write the sequences to a new FASTA file
 
-write.fasta(sequences=seqs[vec.tokeep], names=names(seqs)[vec.tokeep],
-            file.out=paste0('results/subsampled_', serotype, '.fasta'))
-
-# Prepare and write the metadata information table
 seq_name_kept <- as.data.frame(as.matrix(attributes(seqs[vec.tokeep])$names))
 taxa_split <- data.frame(do.call('rbind',strsplit(as.character(seq_name_kept$V1),'|',fixed = TRUE)))
 taxa_split$name <- seq_name_kept$V1
 colnames(taxa_split) <- c('GenBank_ID', "Country", "State",
-                          "City", "Serotype", "Sequence_Type", "date","Decimal_Date","name")
+                          "City", "Serotype", "date","Decimal_Date","name")
 
-taxa_split <- select(taxa_split,c(9,7,1,2,3,4,5,6,8))
+taxa_split <- select(taxa_split,c(8,6,1,2,3,4,5,7))
+write.fasta(sequences=seqs[vec.tokeep], names=names(seqs)[vec.tokeep],
+            file.out=paste0('results/subsampled_', serotype, '.fasta'))
 
-write.table(taxa_split,
-            file = paste0("results/subsampled_",serotype,'_infoTbl.txt'),
-            sep = "\t", 
-            col.names = TRUE, 
-            row.names = FALSE, 
-            quote = FALSE)
+write.csv(taxa_split,
+          file = paste0("results/subsampled_",serotype,'_infoTbl.tsv'),
+          row.names = FALSE, quote=FALSE)
 
 write.csv(taxa_split,
           file = paste0("results/subsampled_",serotype,'_infoTbl.csv'),
-          row.names = FALSE)
+          row.names = FALSE,quote=FALSE)
