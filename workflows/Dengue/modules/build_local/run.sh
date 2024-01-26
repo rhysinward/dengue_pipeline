5d6c3f7792eb8cbfbb4d7d0ba148199fb8e21b95
John Brittain <jsb1982@gmail.com>
2024-01-26 17:23:49 +0000

    Resolve merge conflict

--- workflows/Dengue/modules/build_local/run.sh
+++ workflows/Dengue/modules/build_local/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-CONDA_SUBDIR=osx-64 snakemake --snakefile workflow/Snakefile --cores 4 --use-conda dengue_export_all
+snakemake --snakefile workflow/Snakefile --cores 4 --use-conda dengue_export_all
